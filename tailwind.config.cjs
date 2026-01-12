984f423d6ae364347a1a43f39a8356c6aa69fbad
JesusEstrada2004 <ale2004informacion1819@gmail.com>
2026-01-12 16:26:48 -0600

    Fix Tailwind styles and properly render Card component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
-MediaSourceHandle.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
