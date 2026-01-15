e74c675765f4322c9a37baabff0299ea59bfeff1
JesusEstrada2004 <alejandroestrada7274@gmail.com>
2026-01-15 17:01:12 -0600

    agree new cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
+
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sky: colors.sky,
+        cyan: colors.cyan,
+      },
+    },
   },
   plugins: [],
 }
