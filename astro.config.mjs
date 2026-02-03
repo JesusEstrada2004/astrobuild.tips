224da23f24b7278dd500cc282c9ea4fbc73ced6d
JesusEstrada2004 <ale2004informacion1819@gmail.com>
2026-02-03 15:48:31 -0600

    install svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,14 @@ import mdx from '@astrojs/mdx';
 
 import react from '@astrojs/react';
 
+import vue from '@astrojs/vue';
+
+import svelte from '@astrojs/svelte';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), vue(), svelte()],
 });
